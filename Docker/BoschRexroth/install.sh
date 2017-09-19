6c4f2c65a56e2a3e0b31491ba8a2f91cf4df6fb7
kuf <Klaus.Fischer@dfki.de>
2017-09-19 17:29:46 +0200

    Added Web client WebClientBR.zip in compressed format.

---
+++ Docker/BoschRexroth/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+FIVESHOME=/root/fives
+WEBAPPS=/var/www/html/
+BRHOME=/root/3DVS/BoschRexroth
+
+echo ${FIVESHOME}
+echo ${WEBAPPS}
+
+cp -r ${BRHOME}/CustomizedAnimation ${FIVESHOME}/Plugins/
+cp -r ${BRHOME}/SceneParser ${FIVESHOME}/Plugins/
+
+cp ${BRHOME}/WebClientBR.zip ${WEBAPPS}
+cd $(WEBAPPS}; unzip WebClientBR.zip
+
+cp -f ${WPHOME}/FIVES.sln ${FIVESHOME}
+
+
