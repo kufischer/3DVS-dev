66bf86b4b52a395155929217c030f7f4b0681b23
kuf <Klaus.Fischer@dfki.de>
2017-09-22 11:52:01 +0200

    Added Smart Factory Web App

---
+++ Docker/SmartFactory/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+FIVESHOME=/root/fives
+WEBAPPS=/var/www/html/
+SFHOME=/root/3DVS/SmartFactory
+
+echo ${FIVESHOME}
+echo ${WEBAPPS}
+
+cp ${WPHOME}/SmartFactory.zip ${WEBAPPS}
+cd ${WEBAPPS}; unzip SmartFactory.zip
+
